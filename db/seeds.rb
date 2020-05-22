d0cbce6dfda3509cd120337d3acce86f0deacf16
Barrett Helzel <barrett.p.helzel@gmail.com>
2020-05-21 23:08:48 -0700

    update seedfile

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Question.create!(
+    text: "What year did the Wright Brothers first take flight?",
+    answer: "1903",
+    category: "History",
+    answers_array: ["1915", "1903", "1898", "1936"],
+    difficulty: "Medium",
+    game: "History Buffs"
+)
+
+Question.create!(
+    text: "____________ is an ethical theory that argues right and wrong are determined by a set of universal moral rules, such as 'don't lie, steal, or cheat'.",
+    answer: "Deontology",
+    category: "Philosophy",
+    answers_array: ["Utilitarianism", "Existentialism", "Deontology", "Consequentialism"],
+    difficulty: "Hard",
+    game: "Liberal Arts"
+)
