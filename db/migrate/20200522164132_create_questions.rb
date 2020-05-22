2ba19470b25dd795191c846066d1d6f0e74656ad
Barrett Helzel <barrett.p.helzel@gmail.com>
2020-05-22 09:47:53 -0700

    write question migration

--- db/migrate/20200522164132_create_questions.rb
+++ db/migrate/20200522164132_create_questions.rb
@@ -1,7 +1,12 @@
 class CreateQuestions < ActiveRecord::Migration[6.0]
   def change
     create_table :questions do |t|
-
+      t.string :text, null: false
+      t.string :answer, null: false
+      t.string :category, null: false
+      t.string :answers_array, null: false, array: true
+      t.string :difficulty, null: false
+      t.string :game, null: false
       t.timestamps
     end
   end
