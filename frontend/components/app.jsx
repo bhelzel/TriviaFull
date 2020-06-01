04113c9222bec61167ea44addd29f621b9b0a51f
Barrett Helzel <barrett.p.helzel@gmail.com>
2020-06-01 12:57:46 -0700

    create App component

---
+++ frontend/components/app.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import {
+    Route,
+    Redirect,
+    Switch,
+    Link,
+    HashRouter
+} from 'react-router-dom';
+
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
+
+const App = () => (
+    <div className="wrapper">
+        <Switch>
+
+        </Switch>
+    </div>
+);
+
+export default App;
